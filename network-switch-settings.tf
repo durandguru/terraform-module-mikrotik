510dee9344a172eb170f90283a8ceacac5670b03
durandguru <info@durand.guru>
2024-01-16 18:54:12 +0100

    test vlan

--- network-switch-settings.tf
+++ network-switch-settings.tf
@@ -4,14 +4,6 @@ resource "routeros_interface_ethernet_switch" "switch1" {
   cpu_flow_control = true
 }
 
-# resource "routeros_interface_ethernet_switch_port" "switchport" {
-#   for_each        = var.device_network_settings.ports
-#   name            = each.key
-#   vlan_mode       = "secure"
-#   vlan_header     = "leave-as-is"
-#   default_vlan_id = each.value.vlan == "1" ? "auto" : each.value.vlan
-# }
-
 resource "routeros_interface_ethernet_switch_port" "switchport" {
   for_each = {
     for k, v in var.device_network_settings.ports : k => v
@@ -20,4 +12,13 @@ resource "routeros_interface_ethernet_switch_port" "switchport" {
   vlan_mode       = "secure"
   vlan_header     = "leave-as-is"
   default_vlan_id = each.value.vlan == "1" ? "auto" : each.value.vlan
+}
+
+resource "routeros_interface_ethernet_switch_vlan" "vlan" {
+    for_each = {
+    for k, v in var.device_network_settings.vlans : k => v
+  if v.vlan_if && var.device_settings.vlan_mode == "switch" }
+  switch  = routeros_interface_ethernet_switch.switch1.name
+  ports   = ["ether1", "switch1-cpu"]
+  vlan_id = 99
 }
