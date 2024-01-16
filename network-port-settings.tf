597898f6d42456e69be608114fcd7284fae8010a
durandguru <info@durand.guru>
2024-01-16 17:41:25 +0100

    add vlanid

--- network-port-settings.tf
+++ network-port-settings.tf
@@ -2,7 +2,7 @@ resource "routeros_bridge_port" "bridgeport" {
   for_each          = var.device_network_settings.ports
   bridge            = var.shared_settings.bridge_name
   interface         = each.key
-  pvid              = each.value.vlan
+  pvid              = var.device_settings.vlan_mode == "bridge" ? each.value.vlan : "1"
   ingress_filtering = var.device_settings.vlan_mode == "bridge" ? true : false
   frame_types       = var.device_settings.vlan_mode == "bridge" ? each.value.type : "admit-all"
   hw                = true
