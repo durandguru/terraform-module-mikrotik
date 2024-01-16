bf8a686a3936674f75e5f247d52294031d1753e0
durandguru <info@durand.guru>
2024-01-16 17:58:43 +0100

    add auto vlan

--- network-switch-settings.tf
+++ network-switch-settings.tf
@@ -5,9 +5,9 @@ resource "routeros_interface_ethernet_switch" "switch1" {
 }
 
 resource "routeros_interface_ethernet_switch_port" "switchport" {
-  for_each  = var.device_network_settings.ports
-  name = each.key
-  vlan_mode = "secure"
-  vlan_header = "leave-as-is"
-  default_vlan_id = each.value.vlan
+  for_each        = var.device_network_settings.ports
+  name            = each.key
+  vlan_mode       = "secure"
+  vlan_header     = "leave-as-is"
+  default_vlan_id = each.value.vlan == "1" ? "auto" : each.value.vlan
 }
