c3b296078a7811eb176b7e7359068b46b6189d63
durandguru <info@durand.guru>
2024-01-16 17:51:07 +0100

    switch - part 1

---
+++ network-switch-settings.tf
@@ -0,0 +1,13 @@
+resource "routeros_interface_ethernet_switch" "switch1" {
+  count            = var.device_settings.vlan_mode == "bridge" ? 1 : 0
+  name             = "switch1"
+  cpu_flow_control = true
+}
+
+resource "routeros_interface_ethernet_switch_port" "switchport" {
+  for_each  = var.device_network_settings.ports
+  name = each.key
+  vlan_mode = "secure"
+  vlan_header = "leave-as-is"
+  default_vlan_id = each.value.vlan
+}
