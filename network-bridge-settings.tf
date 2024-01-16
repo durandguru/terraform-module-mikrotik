e78cb829779f178fa9393399568be4f8315744d4
durandguru <info@durand.guru>
2024-01-16 17:24:31 +0100

    add condition

--- network-bridge-settings.tf
+++ network-bridge-settings.tf
@@ -1,6 +1,6 @@
 resource "routeros_bridge" "bridge" {
   name              = var.shared_settings.bridge_name
-  ingress_filtering = true
+  ingress_filtering = var.device_settings.vlan_mode == "bridge" ? true : false
   protocol_mode     = "none"
   priority          = var.device_settings.bridge_priority
   igmp_snooping     = false
