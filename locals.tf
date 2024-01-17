cd374690c99941bce7596b0e2f03d558d15e80f9
durandguru <info@durand.guru>
2024-01-17 15:23:15 +0100

    fmt

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
 
   trunk_ports_switch = keys({
     for k, v in var.device_network_settings.ports : k => v
-  if v.type == "admit-only-vlan-tagged" && v.switch != "no"})
+  if v.type == "admit-only-vlan-tagged" && v.switch != "no" })
 
   hybrid_ports_untagged = {
     for k, v in var.device_network_settings.ports : k => v
