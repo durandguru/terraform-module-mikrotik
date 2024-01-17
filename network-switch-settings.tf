d6c2cef103554fdff452c95ac6cfc965883b2e7b
durandguru <info@durand.guru>
2024-01-17 11:35:54 +0100

    add switch vlan

--- network-switch-settings.tf
+++ network-switch-settings.tf
@@ -14,11 +14,22 @@ resource "routeros_interface_ethernet_switch_port" "switchport" {
   default_vlan_id = each.value.vlan == "1" ? "auto" : each.value.vlan
 }
 
+# resource "routeros_interface_ethernet_switch_vlan" "vlan" {
+#   for_each = {
+#     for k, v in var.device_network_settings.vlans : k => v
+#   if v.vlan_if && var.device_settings.vlan_mode == "switch" }
+#   switch  = "switch1"
+#   ports   = ["ether1", "switch1-cpu"]
+#   vlan_id = each.value.vlan_id
+# }
+
 resource "routeros_interface_ethernet_switch_vlan" "vlan" {
-    for_each = {
-    for k, v in var.device_network_settings.vlans : k => v
-  if v.vlan_if && var.device_settings.vlan_mode == "switch" }
-  switch  = "switch1"
-  ports   = ["ether1", "switch1-cpu"]
-  vlan_id = each.value.vlan_id
+  for_each = var.device_network_settings.vlans
+  switch = "switch1"
+  ports = compact(concat(
+    split(",", replace(replace(contains(local.vlan_interfaces, each.value.vlan_id), "true", "switch1-cpu"), "false", "")),
+    local.trunk_ports,
+    keys({ for k, v in local.hybrid_ports_untagged : k => v if v.vlan != each.value.vlan_id })
+  ))
+  vlan_ids = each.value.vlan_id
 }
