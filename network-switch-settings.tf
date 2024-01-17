e866ed0d013c9e5821e6310d25269ae54dec4f0d
durandguru <info@durand.guru>
2024-01-17 11:46:47 +0100

    test2

--- network-switch-settings.tf
+++ network-switch-settings.tf
@@ -29,7 +29,7 @@ resource "routeros_interface_ethernet_switch_vlan" "vlan" {
   ports = compact(concat(
     split(",", replace(replace(contains(local.vlan_interfaces, each.value.vlan_id), "true", "switch1-cpu"), "false", "")),
     local.trunk_ports,
-    local.untagged_ports
+    keys({ for k, v in local.untagged_ports : k => v if v.vlan != each.value.vlan_id })
   ))
   vlan_id = each.value.vlan_id
 }
