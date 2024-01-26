c7e24708da3c4a2f94c1ed94bc3265449e7705a4
durandguru <info@durand.guru>
2024-01-26 19:41:12 +0100

    Add Poe

---
+++ interface-ethernet.tf
@@ -0,0 +1,8 @@
+resource "routeros_interface_ethernet" "interface" {
+  for_each = {
+    for k, v in var.device_network_settings.ports : k => v
+  if v.poe }
+  factory_name = each.key
+  name         = each.key
+  poe_out      = each.value.vlan
+}
