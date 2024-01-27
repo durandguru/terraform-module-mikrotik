0c8c183d9aef6b84e41956c2202bd6ec6905e13e
durandguru <info@durand.guru>
2024-01-27 13:47:10 +0100

    test7

--- interface-ethernet.tf
+++ interface-ethernet.tf
@@ -10,7 +10,7 @@
 resource "routeros_interface_ethernet" "interface" {
   for_each = {
     for k, v in var.device_network_settings.ports : k => v
-  if contains("ether", k)}
+  if contains(["ether"], k)}
   factory_name = each.key
   name         = each.key
   poe_out      = each.value.poe
