79910e523ef31c07cd51e84a9396deb0428a70ed
durandguru <info@durand.guru>
2024-01-27 13:56:04 +0100

    Fix Poe empty value

--- interface-ethernet.tf
+++ interface-ethernet.tf
@@ -1,21 +1,3 @@
-# resource "routeros_interface_ethernet" "interface" {
-#   for_each = {
-#     for k, v in var.device_network_settings.ports : k => v
-#   if each.key != "sfp1" && v.poe != null }
-#   factory_name = each.key
-#   name         = each.key
-#   poe_out      = each.value.poe
-# }
-
-# resource "routeros_interface_ethernet" "interface" {
-#   for_each = {
-#     for k, v in var.device_network_settings.ports : k => v
-#   if contains(["ether"], k)}
-#   factory_name = each.key
-#   name         = each.key
-#   poe_out      = each.value.poe
-# }
-
 resource "routeros_interface_ethernet" "interface" {
   for_each = {
     for k, v in var.device_network_settings.ports : k => v
