486a698a25010dbcf61403754d041ccede6f71e6
durandguru <info@durand.guru>
2024-01-27 13:41:21 +0100

    test6

--- interface-ethernet.tf
+++ interface-ethernet.tf
@@ -1,7 +1,7 @@
 resource "routeros_interface_ethernet" "interface" {
   for_each = {
     for k, v in var.device_network_settings.ports : k => v
-  if key != "sfp1" && v.poe != null }
+  if each.key != "sfp1" && v.poe != null }
   factory_name = each.key
   name         = each.key
   poe_out      = each.value.poe
