3802cfab27070e23085b6465486181264eb8c553
durandguru <info@durand.guru>
2024-01-27 13:49:48 +0100

    test8

--- interface-ethernet.tf
+++ interface-ethernet.tf
@@ -7,10 +7,19 @@
 #   poe_out      = each.value.poe
 # }
 
+# resource "routeros_interface_ethernet" "interface" {
+#   for_each = {
+#     for k, v in var.device_network_settings.ports : k => v
+#   if contains(["ether"], k)}
+#   factory_name = each.key
+#   name         = each.key
+#   poe_out      = each.value.poe
+# }
+
 resource "routeros_interface_ethernet" "interface" {
   for_each = {
     for k, v in var.device_network_settings.ports : k => v
-  if contains(["ether"], k)}
+  if can(v.poe)}
   factory_name = each.key
   name         = each.key
   poe_out      = each.value.poe
