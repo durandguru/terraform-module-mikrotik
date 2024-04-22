ba78aad5bbb2a99fc18b776974f55b16ff0b8c02
durandguru <info@durand.guru>
2024-04-22 13:08:16 +0200

    To Lowercase

--- ip_neighbor_discovery_settings.tf
+++ ip_neighbor_discovery_settings.tf
@@ -1,5 +1,5 @@
 resource "routeros_ip_neighbor_discovery_settings" "nds" {
   discover_interface_list = routeros_interface_list.mgmt.name
   mode                    = "tx-and-rx"
-  protocol                = ["CDP", "LLDP", "MNDP"]
+  protocol                = ["cdp", "lldp", "mndp"]
 }
