08d56b75cb02381aec82e969718b223a96116ef0
durandguru <info@durand.guru>
2024-05-17 18:19:21 +0200

    Add TZ

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     routeros = {
       source  = "terraform-routeros/routeros"
-      version = "1.48.1"
+      version = "1.49.0"
     }
   }
 }
---
+++ routeros_system_clock.tf
@@ -0,0 +1,3 @@
+resource "routeros_system_clock" "set" {
+    time_zone_autodetect = true
+}
