10850a3532c81a406b32902f0390c13624123136
durandguru <info@durand.guru>
2024-05-30 11:42:22 +0200

    Add SCEP

--- routeros-system-certificate.tf
+++ routeros-system-certificate.tf
@@ -9,7 +9,24 @@ resource "routeros_system_certificate" "root_ca" {
   state        = "Gelderland"
   unit         = "Omnisport"
   days_valid   = "3650"
+  key_size     = "4096"
   # Sign Root CA.
   sign {
   }
+}
+
+resource "routeros_system_certificate_scep_server" "scep-90-days" {
+  ca_cert    = routeros_system_certificate.root_ca.name
+  path       = "/scep/90"
+  days_valid = 90
+
+  depends_on = [routeros_system_certificate.root_ca]
+}
+
+resource "routeros_system_certificate_scep_server" "scep-365-days" {
+  ca_cert    = routeros_system_certificate.root_ca.name
+  path       = "/scep/365"
+  days_valid = 365
+
+  depends_on = [routeros_system_certificate.root_ca]
 }
