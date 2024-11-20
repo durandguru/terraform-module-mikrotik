6cf1df72e3c4fb2eda015c9ca6b41523565aaa5e
Sebastiaan Durand <info@durand.guru>
2024-11-20 13:03:37 +0100

    add cname dns

--- general-ip-dns-records.tf
+++ general-ip-dns-records.tf
@@ -1,8 +1,17 @@
-resource "routeros_ip_dns_record" "dns-record" {
+resource "routeros_ip_dns_record" "dns-record-a" {
   for_each = {
     for k, v in var.environment_network_settings.dns-records : k => v
-  if var.device_settings.device_type == "router" }
+  if var.device_settings.device_type == "router" && v.dns_type == "A"}
   type    = each.value.dns_type
   address = each.value.dns_address
+  name    = each.value.dns_name
+}
+
+resource "routeros_ip_dns_record" "dns-record-cname" {
+  for_each = {
+    for k, v in var.environment_network_settings.dns-records : k => v
+  if var.device_settings.device_type == "router" && v.dns_type == "CNAME" }
+  type    = each.value.dns_type
+  cname   = each.value.dns_address
   name    = each.value.dns_name
 }
