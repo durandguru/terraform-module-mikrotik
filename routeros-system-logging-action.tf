1238b87196a171aed41af660e2143942437da038
Sebastiaan Durand <info@durand.guru>
2025-05-10 07:21:47 +0200

    disable bsd_syslog

--- routeros-system-logging-action.tf
+++ routeros-system-logging-action.tf
@@ -1,7 +1,7 @@
 resource "routeros_system_logging_action" "syslog" {
   name            = "remote"
   target          = "remote"
-  bsd_syslog      = true
+  # bsd_syslog      = true
   remote          = var.shared_settings.syslog_server
   remote_port     = "514"
   src_address     = var.device_settings.ip
