12f5bd8e84293cca43ecadeac92a44e923071b6e
Sebastiaan Durand <info@durand.guru>
2025-05-13 21:09:15 +0200

    change ip cloud to auto

--- general-ip-cloud.tf
+++ general-ip-cloud.tf
@@ -1,4 +1,4 @@
 resource "routeros_ip_cloud" "settings" {
   ddns_enabled = var.device_settings.device_type == "router" && var.device_settings.environment == "maarschalkerweerd" ? true : false
-  update_time  = false
+  update_time  = "auto"
 }
