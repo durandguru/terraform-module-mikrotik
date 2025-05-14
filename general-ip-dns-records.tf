9aec382a2b910b5ab12ba4488d47b0efa3424285
Sebastiaan Durand <info@durand.guru>
2025-05-14 20:21:04 +0200

    add ipv6 records

--- general-ip-dns-records.tf
+++ general-ip-dns-records.tf
@@ -7,6 +7,15 @@ resource "routeros_ip_dns_record" "dns-record-a" {
   name    = each.value.dns_name
 }
 
+resource "routeros_ip_dns_record" "dns-record-aaaa" {
+  for_each = {
+    for k, v in var.environment_network_settings.dns-records : k => v
+  if var.device_settings.device_type == "router" && v.dns_type == "AAAA"}
+  type    = each.value.dns_type
+  address = each.value.dns_address
+  name    = each.value.dns_name
+}
+
 resource "routeros_ip_dns_record" "dns-record-cname" {
   for_each = {
     for k, v in var.environment_network_settings.dns-records : k => v
