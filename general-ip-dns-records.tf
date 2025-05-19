4d48a14f2804e33150c08b1a33083fd3ea9579bb
Sebastiaan Durand <info@durand.guru>
2025-05-19 14:12:25 +0200

    add fwd records

--- general-ip-dns-records.tf
+++ general-ip-dns-records.tf
@@ -1,7 +1,7 @@
 resource "routeros_ip_dns_record" "dns-record-a" {
   for_each = {
     for k, v in var.environment_network_settings.dns-records : k => v
-  if var.device_settings.device_type == "router" && v.dns_type == "A"}
+  if var.device_settings.device_type == "router" && v.dns_type == "A" }
   type    = each.value.dns_type
   address = each.value.dns_address
   name    = each.value.dns_name
@@ -10,7 +10,7 @@ resource "routeros_ip_dns_record" "dns-record-a" {
 resource "routeros_ip_dns_record" "dns-record-aaaa" {
   for_each = {
     for k, v in var.environment_network_settings.dns-records : k => v
-  if var.device_settings.device_type == "router" && v.dns_type == "AAAA"}
+  if var.device_settings.device_type == "router" && v.dns_type == "AAAA" }
   type    = each.value.dns_type
   address = each.value.dns_address
   name    = each.value.dns_name
@@ -20,7 +20,16 @@ resource "routeros_ip_dns_record" "dns-record-cname" {
   for_each = {
     for k, v in var.environment_network_settings.dns-records : k => v
   if var.device_settings.device_type == "router" && v.dns_type == "CNAME" }
-  type    = each.value.dns_type
-  cname   = each.value.dns_address
-  name    = each.value.dns_name
+  type  = each.value.dns_type
+  cname = each.value.dns_address
+  name  = each.value.dns_name
+}
+
+resource "routeros_ip_dns_record" "dns-record-fwd" {
+  for_each = {
+    for k, v in var.environment_network_settings.dns-records : k => v
+  if var.device_settings.device_type == "router" && v.dns_type == "FWD" }
+  forward-to = each.value.dns_forward_to
+  type       = each.value.dns_type
+  regexp     = each.value.dns_regexp
 }
