cc6638b1379e9bf2fea593faac6b0398ff2598ca
Sebastiaan Durand <info@durand.guru>
2025-05-19 14:08:12 +0200

    remove bsd

--- routeros-system-logging-action.tf
+++ routeros-system-logging-action.tf
@@ -1,7 +1,6 @@
 resource "routeros_system_logging_action" "syslog" {
-  name            = "remote"
-  target          = "remote"
-  # bsd_syslog      = true
+  name   = "remote"
+  target = "remote"
   remote          = var.shared_settings.syslog_server
   remote_port     = "514"
   src_address     = var.device_settings.ip
