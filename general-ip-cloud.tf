29f86605816664fd477165201ea3dab101234db4
Sebastiaan Durand <info@durand.guru>
2025-05-19 13:19:33 +0200

    change values

--- general-ip-cloud.tf
+++ general-ip-cloud.tf
@@ -1,4 +1,4 @@
 resource "routeros_ip_cloud" "settings" {
-  ddns_enabled = var.device_settings.device_type == "router" && var.device_settings.environment == "maarschalkerweerd" ? "auto" : false
+  ddns_enabled = var.device_settings.device_type == "router" && var.device_settings.environment == "maarschalkerweerd" ? "yes" : "auto"
   update_time  = false
 }
