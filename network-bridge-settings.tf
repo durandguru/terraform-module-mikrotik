cc862fe009e1820d2737ad43da23a9017a13f468
Sebastiaan Durand <info@durand.guru>
2025-05-23 15:32:24 +0200

    add igmp settings

--- network-bridge-settings.tf
+++ network-bridge-settings.tf
@@ -3,6 +3,10 @@ resource "routeros_interface_bridge" "bridge" {
   ingress_filtering = var.device_settings.vlan_mode == "bridge" ? true : false
   protocol_mode     = "none"
   priority          = var.device_settings.bridge_priority
-  igmp_snooping     = false
+  igmp_snooping     = true
+  igmp_version      = "3"
+  mld_version       = "2"
+  multicast_router  = var.device_settings.device_type  == "router" ? "Permananent" : "Temporary Query"
+  multicast_querier = var.device_settings.device_type  == "router" ? true : false
   vlan_filtering    = var.device_settings.vlan_mode == "bridge" ? true : false
 }
