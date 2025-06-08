95da410271d1708d3ad5267eea76ff7cbe1b5ac4
durandguru <info@durand.guru>
2025-06-08 20:34:22 +0200

    disable omnisport routes

--- general-ip-routes.tf
+++ general-ip-routes.tf
@@ -7,20 +7,20 @@ resource "routeros_ip_route" "route-default" {
   distance      = "1"
 }
 
-resource "routeros_ip_route" "route-omnisport" {
-  count         = var.device_settings.device_type != "router" && var.device_settings.environment == "event" ? 1 : 0
-  dst_address   = "0.0.0.0/0"
-  gateway       = "172.22.10.2"
-  comment       = "Default Route to Internet via NL-OMNISPORT"
-  check_gateway = "ping"
-  distance      = "2"
-}
+# resource "routeros_ip_route" "route-omnisport" {
+#   count         = var.device_settings.device_type != "router" && var.device_settings.environment == "event" ? 1 : 0
+#   dst_address   = "0.0.0.0/0"
+#   gateway       = "172.22.10.2"
+#   comment       = "Default Route to Internet via NL-OMNISPORT"
+#   check_gateway = "ping"
+#   distance      = "2"
+# }
 
-resource "routeros_ip_route" "route-fw4011" {
-  count         = var.device_settings.device_type != "router" && var.device_settings.environment == "event" ? 1 : 0
-  dst_address   = "0.0.0.0/0"
-  gateway       = "172.22.10.3"
-  comment       = "Default Route to Internet via NL-EVENT-4011"
-  check_gateway = "ping"
-  distance      = "3"
-}
+# resource "routeros_ip_route" "route-fw4011" {
+#   count         = var.device_settings.device_type != "router" && var.device_settings.environment == "event" ? 1 : 0
+#   dst_address   = "0.0.0.0/0"
+#   gateway       = "172.22.10.3"
+#   comment       = "Default Route to Internet via NL-EVENT-4011"
+#   check_gateway = "ping"
+#   distance      = "3"
+# }
